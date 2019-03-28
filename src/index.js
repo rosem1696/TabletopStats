377735c0497138b16a047bd890600160de745f60
Mitchell Rose <rosem1696@gmail.com>
2019-03-28 15:27:53 -0500

    webpack basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+//force webpack to copy index.html
+require('file-loader?name=[name].[ext]!./index.html');
+
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 var path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'tbtstats.bundle.js'
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'tbtstats.bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ]
+    }
 };
