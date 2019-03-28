aa1987cd44efed2490f392df168ad68d02b478e0
Mitchell Rose <rosem1696@gmail.com>
2019-03-28 15:55:05 -0500

    added basic eslint config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('file-loader?name=[name].[ext]!./index.html');
 
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+//import Icon from './icon.png';
 
 function component() {
     let element = document.createElement('div');
@@ -11,8 +11,8 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
 
     element.appendChild(myIcon);
 
